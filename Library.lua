7dacfdffc8d550dc55fd7b71760deacd67ac4bca
4everVortex <119734721+4everVortex@users.noreply.github.com>
2022-12-13 22:47:43 +0700

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -32,7 +32,7 @@ local Library = {
     MainColor = Color3.fromRGB(0, 15, 30);
     BackgroundColor = Color3.fromRGB(5, 5, 20);
     AccentColor = Color3.fromRGB(0, 255, 187);
-    OutlineColor = Color3.fromRGB(0, 0, 5);
+    OutlineColor = Color3.fromRGB(38, 38, 38);
 
     Black = Color3.new(0, 0, 0);
 
